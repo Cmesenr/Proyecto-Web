55f7ff4b0cd23f7fa815314bbcc3572e00f1cc70
AngelMF <amunguia19@hotmail.com>
2015-10-18 12:25:02 -0600

    Mantenimiento Cliente y agregue campo dirección en la lista index de los mantenimientos cliente y proveedor

---
+++ SWRCVA/SWRCVA/Controllers/ClienteController.cs
@@ -0,0 +1,187 @@
+﻿using SWRCVA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PagedList;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Data.Entity.Infrastructure;
+
+namespace SWRCVA.Controllers
+{
+    public class ClienteController : Controller
+    {
+        DataContext db = new DataContext();
+
+        // GET: Cliente
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Nombre" : "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var clientes = from s in db.Cliente
+                              select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                clientes = clientes.Where(s => s.Nombre.Contains(searchString)
+                                                || s.Correo.Contains(searchString)
+                                                || s.Direccion.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "Nombre":
+                    clientes = clientes.OrderByDescending(s => s.Nombre);
+                    break;
+                default:  // Name ascending 
+                    clientes = clientes.OrderBy(s => s.Nombre);
+                    break;
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(clientes.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Cliente/Detalles/5
+        public ActionResult Detalles(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // GET: Cliente/Registrar
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        // POST: Cliente/Registrar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registrar([Bind(Include = "IdCliente, Nombre, Telefono, Correo, Direccion, Estado, Usuario")]Cliente cliente)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (RetryLimitExceededException /* dex */)
+            {
+                ModelState.AddModelError("", "Imposible guardar cambios. Intentelo de nuevo, y si el problema persiste contacte el administrador del sistema.");
+            }
+            return View(cliente);
+        }
+
+        // GET: Cliente/Editar/5
+        public ActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Cliente/Editar/5
+        [HttpPost, ActionName("Editar")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditarPost(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente clienteToUpdate = db.Cliente.Find(id);
+            if (TryUpdateModel(clienteToUpdate, "",
+               new string[] { "Nombre", "Telefono", "Correo", "Direccion", "Estado", "Usuario" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (RetryLimitExceededException /* dex */)
+                {
+                    ModelState.AddModelError("", "Imposible guardar los cambios. Intentelo de nuevo, si el problema persiste, contacte el administrador del sistema.");
+                }
+            }
+            return View(clienteToUpdate);
+        }
+
+        // GET: Cliente/Borrar/5
+        public ActionResult Borrar(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Borrado fallido. Intente de nuevo, y si el problema persiste contacte el administrador del sistema.";
+            }
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Cliente/Borrar/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Borrar(int id)
+        {
+            try
+            {
+                Cliente cliente = db.Cliente.Find(id);
+                db.Cliente.Remove(cliente);
+                db.SaveChanges();
+            }
+            catch (RetryLimitExceededException/* dex */)
+            {
+                return RedirectToAction("Borrar", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- SWRCVA/SWRCVA/Models/Cliente.cs
+++ SWRCVA/SWRCVA/Models/Cliente.cs
@@ -23,9 +23,11 @@ namespace SWRCVA.Models
         public string Nombre { get; set; }
 
         [Column(TypeName = "numeric")]
-        public decimal? Telefono { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0}")]
+        public decimal Telefono { get; set; }
 
         [StringLength(30)]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Correo no válido")]
         public string Correo { get; set; }
 
         [StringLength(30)]
