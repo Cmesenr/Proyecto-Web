2e2f0dbdc269588d93d654f8c598269943da3684
Cmesen <charlie.carlos.mesn@gmail.com>
2015-10-18 10:53:32 -0600

    a ver si se arregla

--- SWRCVA/SWRCVA/Controllers/MaterialController.cs
+++ SWRCVA/SWRCVA/Controllers/MaterialController.cs
@@ -15,10 +15,7 @@ namespace SWRCVA.Controllers
         {
 
 
-            ViewBag.CatMaterial = new SelectList(db.CategoriaMat, "IdCategoria", "Nombre");
-            ViewBag.ColorMaterial = new SelectList(db.ColorMat,"IdColor","Nombre");
-            ViewBag.SubCatMaterial = new SelectList(db.SubCategoria, "IdSubCatMat", "Nombre");
-            ViewBag.Proveedor = new SelectList(db.Proveedor, "IdProveedor", "Nombre");
+            
             return View();
         }
 
@@ -29,8 +26,12 @@ namespace SWRCVA.Controllers
         }
 
         // GET: Material/Create
-        public ActionResult Create()
+        public ActionResult Registrar()
         {
+            ViewBag.CatMaterial = new SelectList(db.CategoriaMat, "IdCategoria", "Nombre");
+            ViewBag.ColorMaterial = new SelectList(db.ColorMat, "IdColor", "Nombre");
+            ViewBag.SubCatMaterial = new SelectList(db.SubCategoria, "IdSubCatMat", "Nombre");
+            ViewBag.Proveedor = new SelectList(db.Proveedor, "IdProveedor", "Nombre");
             return View();
         }
 
