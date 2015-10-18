a4313ec8fb86d019fe6a2895f80d17f3014c42b3
Cmesen <charlie.carlos.mesn@gmail.com>
2015-10-18 10:56:43 -0600

    Revert "a ver si se arregla"
    This reverts commit 2e2f0dbdc269588d93d654f8c598269943da3684.

--- SWRCVA/SWRCVA/Controllers/MaterialController.cs
+++ SWRCVA/SWRCVA/Controllers/MaterialController.cs
@@ -15,7 +15,10 @@ namespace SWRCVA.Controllers
         {
 
 
-            
+            ViewBag.CatMaterial = new SelectList(db.CategoriaMat, "IdCategoria", "Nombre");
+            ViewBag.ColorMaterial = new SelectList(db.ColorMat,"IdColor","Nombre");
+            ViewBag.SubCatMaterial = new SelectList(db.SubCategoria, "IdSubCatMat", "Nombre");
+            ViewBag.Proveedor = new SelectList(db.Proveedor, "IdProveedor", "Nombre");
             return View();
         }
 
@@ -26,12 +29,8 @@ namespace SWRCVA.Controllers
         }
 
         // GET: Material/Create
-        public ActionResult Registrar()
+        public ActionResult Create()
         {
-            ViewBag.CatMaterial = new SelectList(db.CategoriaMat, "IdCategoria", "Nombre");
-            ViewBag.ColorMaterial = new SelectList(db.ColorMat, "IdColor", "Nombre");
-            ViewBag.SubCatMaterial = new SelectList(db.SubCategoria, "IdSubCatMat", "Nombre");
-            ViewBag.Proveedor = new SelectList(db.Proveedor, "IdProveedor", "Nombre");
             return View();
         }
 
