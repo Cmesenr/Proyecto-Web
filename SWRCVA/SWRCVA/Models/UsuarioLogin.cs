60acbc101fcaa6851c048b429ac83fe07d7de3fa
AngelMF <amunguia19@hotmail.com>
2015-10-23 20:34:15 -0600

    Prueba para que Charlie corra una nueva pagina

---
+++ SWRCVA/SWRCVA/Models/UsuarioLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWRCVA.Models
+{
+    public partial class UsuarioLogin
+    {
+        public UsuarioLogin()
+        {
+
+        }
+
+        public string UserId { get; set; }
+
+        public string Password { get; set; }
+    }
+}
