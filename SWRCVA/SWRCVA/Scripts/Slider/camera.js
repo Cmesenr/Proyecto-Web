742c174c23809dfaba96a0d43c68ca0ab7b5309e
Charlie Mesen <charlie.carlos.mesn@gmail.com>
2015-11-01 16:20:13 -0600

    Botones slider

--- SWRCVA/SWRCVA/Scripts/Slider/camera.js
+++ SWRCVA/SWRCVA/Scripts/Slider/camera.js
@@ -39,7 +39,7 @@
 		
 		loaderBgColor		: '#222222', 
 		
-		loaderOpacity		: .8,	//0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1
+		loaderOpacity		: .1,	//0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1
 		
 		loaderPadding		: 2,	//how many empty pixels you want to display between the loader and its background
 		
