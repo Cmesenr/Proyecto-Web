20861a6814b5b57216a9616367d890393842498c
AngelMF <amunguia19@hotmail.com>
2015-11-10 22:09:21 -0600

    Trabajando orden a como lo entendi

--- SWRCVA/SWRCVA/Scripts/FuncionesOrden.js
+++ SWRCVA/SWRCVA/Scripts/FuncionesOrden.js
@@ -1,8 +1,22 @@
-﻿function Editar(valor) {
-    var params = { id: valor };
+﻿function Editar(idCotizacion,idProducto) {
+    var params = { idCotizacion: idCotizacion, idProducto:idProducto };
     $.ajax({
         cache: false,
         url: "/Orden/Editar",
+        type: "GET",
+        data: params,
+        contentType: "application/json; charset=utf-8",
+        success: function (result) {
+            parent.document.location = parent.document.location;
+        }
+    });
+}
+
+function AlmacenarEstado(estado) {
+    var params = { estado: estado };
+    $.ajax({
+        cache: false,
+        url: "/Orden/AlmacenarEstado",
         type: "GET",
         data: params,
         contentType: "application/json; charset=utf-8",
