d3171350b4806714c292b79c7675bae80a911ed9
AngelMF <amunguia19@hotmail.com>
2015-11-22 16:06:49 -0600

    Trabajando un reporte, mae reviselo y me dice que ve mal hasta lo que llevo

---
+++ SWRCVA/SWRCVA/Models/Reportes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SWRCVA.Models
+{
+    public class Reportes
+    {
+        private DataContext db = new DataContext();
+
+        public DataTable reporteCotizacion(DateTime fecha)
+        {
+            string pConsulta = "set dateformat dmy; exec sp_getDatosReporteCotizacion '" + fecha.ToShortDateString() + "'";
+
+            DataTable dt = new DataTable();
+            SqlConnection conn = ((SqlConnection)db.Database.Connection);
+            conn.Open();
+
+            System.Data.SqlClient.SqlDataAdapter adaptador = new System.Data.SqlClient.SqlDataAdapter(pConsulta, conn);
+            System.Data.DataTable dataTable = new System.Data.DataTable();
+            adaptador.Fill(dt);
+
+            conn.Close();
+
+            return dt;
+        }
+    }
+}
