35c4ce1169fb3272f1a622c659cfe7a493e53454
Cmesen <charlie.carlos.mesn@gmail.com>
2015-11-29 14:27:53 -0600

    Cambios En cotizacion

---
+++ SWRCVA/SWRCVA/Models/ListaProducto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWRCVA.Models
+{
+    public class ListaProducto
+    {
+        public string Nombre { set; get; }
+        public decimal Cantidad { set; get; }
+        public decimal SubTotal { set; get; }
+    }
+}
